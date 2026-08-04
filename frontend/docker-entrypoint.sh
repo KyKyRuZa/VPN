e68e53e8d051ca1967fa0bf39d30b93f48991cc0
NABE5058118 <galimovkamil613@gmail.com>
2026-08-04 19:44:48 +0300

    fix: prevent entrypoint from exiting when dist is empty

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 if [ -d "/app/dist-fresh" ]; then
-  rm -rf /app/dist/*
+  rm -rf /app/dist/* /app/dist/.* 2>/dev/null || true
   cp -r /app/dist-fresh/. /app/dist/
 fi
 exec "$@"
