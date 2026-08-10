d8fb1afb765e1056e4af39e828677cde274c59fd
NABE5058118 <galimovkamil613@gmail.com>
2026-08-10 21:00:46 +0300

    public key вместо него -pbk

--- backend/internal/services/x3dxui.go
+++ backend/internal/services/x3dxui.go
@@ -399,7 +399,7 @@ func (s *X3dxuiService) BuildVLESSConfig(ctx context.Context, username, uuid str
 	}
 
 	link := fmt.Sprintf(
-		"vless://%s@%s:%d?encryption=none&flow=xtls-rprx-vision&host=%s&mode=packet-up&path=&publicKey=%s&security=reality&shortId=%s&serverName=%s&spx=%s&type=xhttp&x_padding_bytes=100-1000#%s",
+		"vless://%s@%s:%d?encryption=none&flow=xtls-rprx-vision&host=%s&mode=packet-up&path=&pbk=%s&security=reality&sid=%s&sni=%s&spx=%s&type=xhttp&x_padding_bytes=100-1000#%s",
 		uuid,
 		host,
 		port,
